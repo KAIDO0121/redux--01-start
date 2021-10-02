31f9c1bc44f8ad7fe971d8315e4fc5dc74818494
Sean <kaido0121@gmail.com>
2021-10-02 10:10:15 +0800

    using ans to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-it('renders without crashing', () => {
-  expect(1).toBe(1)
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, combineReducers} from 'redux';
-import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
-import counterReducer from './store/reducers/counter'
-import resultReducer from './store/reducers/result'
+import * as serviceWorker from './serviceWorker';
 
-const rootReducer = combineReducers({
-    ctr:counterReducer,
-    res:resultReducer
-})
-const store = createStore(rootReducer);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
